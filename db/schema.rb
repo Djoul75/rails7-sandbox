7cf7cd6f2934f186d951e080339b263e637b4151
Jules Gorce <jules.gorce@gmail.com>
2022-08-29 20:08:51 +0200

    add columns to question model & prepare rails API for update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_123031) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_175527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_123031) do
     t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0
+    t.integer "dislikes_count", default: 0
   end
 
 end
