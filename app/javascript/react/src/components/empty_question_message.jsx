fb3a7d710774e98de60e3d7df6fadeaf4a0c9107
Jules Gorce <jules.gorce@gmail.com>
2022-08-30 16:43:00 +0200

    fix bug when close alert message and select an other tag

--- app/javascript/react/src/components/empty_question_message.jsx
+++ app/javascript/react/src/components/empty_question_message.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 
 const EmptyQuestionMessage = (props) => {
   return(
-    <div className=" mt-5 alert alert-warning alert-dismissible fade show" role="alert">
-      <strong>OOPs!</strong> No questions found with the tag: {props.tagname}. Please select an other option from the list.
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div>
+      <div className=" mt-5 alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>OOPs!</strong> No questions found with the tag: {props.tagname}. Please select an other option from the list.
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   )
 }
